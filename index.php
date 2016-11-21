894d9bd3927459bbee2a1885a3944768a461dca5
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-21 15:03:21 +0000

    updated index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 //dynamic main page
+<!-- Distinct login page for site - existing users-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Events Form</title>
+</head>
 
+<body>
+
+<form action="../scripts/checkLogin.php" method="post">
+    <input type="text" placeholder="User Name" name="userName">
+    <input type="text" placeholder="Password" name="password">
+    <input type="submit" value='Go Go Go!'>
+</form>
+
+</body>
+</html>
 <?php
 /**
  * Created by PhpStorm.
