145e12e710beb900efc7c70419d931a2b5d1d9a1
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-21 15:20:33 +0000

    updated dbconnect.php

--- inc/scripts/dbconnect.php
+++ inc/scripts/dbconnect.php
@@ -1,11 +1,15 @@
 //php file to connect to server
 
 <?php
-define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
-define('DB_USERNAME', 'b1fa144aa688ff');
-define('DB_PASSWORD', '4e96e436');
-define('DB_DATABASE', 'db_pgo_cs7');
-$db = mysqli_connect(DB_SERVER,
-    DB_USERNAME, DB_PASSWORD,
-    DB_DATABASE);
+
+define($DB_SERVER, 'eu-cdbr-azure-north-e.cloudapp.net');
+define($DB_USERNAME, 'b1fa144aa688ff');
+define($DB_PASSWORD, '4e96e436');
+define($DB_DATABASE, 'db_pgo_cs7');
+
+$db = new mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+
+if ($db->connect_errno){
+    die ('Connection Failed :'.$db->connect_error);
+}
 ?>
