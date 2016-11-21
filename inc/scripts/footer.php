fecddc9fedfc2455723fe2fe9879abbec6bc243e
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-21 16:34:58 +0000

    Add a footer

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -1,9 +1,12 @@
-//Page footer
-
-<?php
-/**
- * Created by PhpStorm.
- * User: hype_
- * Date: 07/11/2016
- * Time: 12:06
- */?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>header</title>
+</head>
+<body>
+    <footer>
+        <p>(c)2016 - Charlie Bowler, Alexander Fyfe, William Haynes, Scott McMahon</p>
+        <p>Contact Details</p>
+    </footer>
+</body>
