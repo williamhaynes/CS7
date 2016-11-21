56349c2123947f5e9e88ec9eb9b0de6d6fe5c506
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-21 15:40:17 +0000

    updated index.php with hyperlink

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 //dynamic main page
 <!-- Distinct login page for site - existing users-->
+<!DOCTYPE html>
+    <html>
+    <head>
+
+    </head>
+    <body>
+        <a href="inc\UserPages\loginPage.php">loginPage</a>
+    </body>
+    </html>
 <?php
 
 ?>
