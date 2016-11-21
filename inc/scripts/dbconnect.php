2792eb7bb80062a5a863e3476d101b0763c25aaa
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-21 15:25:53 +0000

    updated dbconnect.php

--- inc/scripts/dbconnect.php
+++ inc/scripts/dbconnect.php
@@ -2,12 +2,12 @@
 
 <?php
 
-define($DB_SERVER, 'eu-cdbr-azure-north-e.cloudapp.net');
-define($DB_USERNAME, 'b1fa144aa688ff');
-define($DB_PASSWORD, '4e96e436');
-define($DB_DATABASE, 'db_pgo_cs7');
-
-$db = new mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+// connect to your Azure server and select database (remember you connection details are all on the azure portal
+$db = new mysqli(
+    "eu-cdbr-azure-north-e.cloudapp.net",
+    "b1fa144aa688ff",
+    "4e96e436",
+    "db_pgo_cs7" );
 
 if ($db->connect_errno){
     die ('Connection Failed :'.$db->connect_error);
