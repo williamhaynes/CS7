2c1f14f7359586c5f39450ae4165dd91340871dc
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-21 15:32:44 +0000

    updated index.php updated web.config

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
 //dynamic main page
 <!-- Distinct login page for site - existing users-->
 <?php
-/**
- * Created by PhpStorm.
- * User: hype_
- * Date: 07/11/2016
- * Time: 12:04
- */
+define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
+$rules = array(
+    //
+    //main pages
+    //
+    'clubsAndSocietiesPage' => "/clubsAndSocietiesPage",
+    'healthAndWellbeing' => "/healthAndWellbeing",
+    'mapPage' => "/mapPage",
+    //
+    //Admin Pages
+    //
+    'login' => "/login",
+    'create_article' => "/createarticle",
+    'logout' => "/logout",
+    //
+    // Home Page
+    //
+    'home' => "/"
+);
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+$uri = '/' . trim(str_replace($uri,
+        ''
+        , $_SERVER['REQUEST_URI']), '/');
+$uri = urldecode($uri);
+foreach ($rules as $action => $rule) {
+    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+        include(INCLUDE_DIR . $action . '.php');
+        exit();
+    }
+}
+// nothing is found so handle the 404 error
+include(INCLUDE_DIR . '404.php');
+?>
