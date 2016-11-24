4258c288e63a3f17ee3c510fed797ce74b307dc7
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-24 15:02:23 +0000

    updating header to remove logo size

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -13,7 +13,7 @@ session_start();
     <header>
         <nav id="navBar">
             <ul id="header">
-                <li id="logo"><img id="logoIMG" src="https://pmcdeadline2.files.wordpress.com/2016/07/logo-tv-logo.png" alt="Logo" style="width:304px;height:228px;"></li>
+                <li id="logo"><img id="logoIMG" src="https://pmcdeadline2.files.wordpress.com/2016/07/logo-tv-logo.png" alt="Logo"></li>
                 <li id="healthAndWellbeing"><a href="/inc/healthAndWellbeing.php">Health & Wellbeing</a></li>
                 <li id="clubsAndSocieties"><a href="/inc/clubsAndSocietiesPage.php">Clubs & Societies</a></li>
                 <li id="mapPage"><a href="/inc/mapPage.php">Maps</a></li>
