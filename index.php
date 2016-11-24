23e4283c05420315ece1f01ac1a4f5c6df16fb4e
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-24 14:03:06 +0000

    Fixing index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <a href="\inc\UserPages\loginPage.php">loginPage</a>
         <a href="\inc\scripts\header.php">header</a>
         <a href="\inc\clubPage.php">clubPage</a>
-        <a href="\inc\clubsAndSocieties.php">clubsAndSocieties</a>
+        <a href="\inc\clubsAndSocietiesPage.php">clubsAndSocietiesPage</a>
         <a href="\inc\healthAndWellbeing.php">healthAndWellbeing</a>
         <a href="\inc\mapPage.php">mapPage</a>
         <p>modification to test branch usage</p>
