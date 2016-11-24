5bff4ff67be49befea83787815c4a86a6334ccb9
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-24 14:38:37 +0000

    Updating link to CSS

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -6,7 +6,7 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="CS7/style/styles.css">
+    <link rel="stylesheet" type="text/css" href="/style/styles.css">
     <title>header</title>
 </head>
 <body>
@@ -14,9 +14,9 @@ session_start();
         <nav id="navBar">
             <ul id="header">
                 <li id="logo"><img src="https://pmcdeadline2.files.wordpress.com/2016/07/logo-tv-logo.png" alt="Logo" style="width:304px;height:228px;"></li>
-                <li id="healthAndWellbeing"><a href="\healthAndWellbeingPage.php">Health & Wellbeing</a></li>
-                <li id="clubsAndSocieties"><a href="\clubsAndSocietiesPage.php">Clubs & Societies</a></li>
-                <li id="mapPage"><a href="\mapPage.php">Maps</a></li>
+                <li id="healthAndWellbeing"><a href="/healthAndWellbeingPage.php">Health & Wellbeing</a></li>
+                <li id="clubsAndSocieties"><a href="/clubsAndSocietiesPage.php">Clubs & Societies</a></li>
+                <li id="mapPage"><a href="/mapPage.php">Maps</a></li>
                 <!-- Code that could be useful for specific users
                  <?
                 if (isset($_SESSION['username'])) {
