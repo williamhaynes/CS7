adbfaaa98128a616535d675b7273fcf8586ea9f9
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-25 10:55:01 +0000

    modified index.php - added access to health and wellbeing form to index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <a href="\inc\clubPage.php">clubPage</a>
         <a href="\inc\clubsAndSocietiesPage.php">clubsAndSocietiesPage</a>
         <a href="\inc\healthAndWellbeing.php">healthAndWellbeing</a>
+        <a href="\inc\Forms\healthAndWellbeingForm.php">Health And Wellbeing Form</a>
         <a href="\inc\mapPage.php">mapPage</a>
         <p>modification to test branch usage</p>
     <p>hello world</p>
