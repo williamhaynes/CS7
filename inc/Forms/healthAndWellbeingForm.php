fa4d59f4f4be78f9e93c9eb128296e4e11896bcf
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-25 10:52:35 +0000

    modified health & wellbeing form, used code from creating ACMS lecture

--- inc/Forms/healthAndWellbeingForm.php
+++ inc/Forms/healthAndWellbeingForm.php
@@ -1,4 +1,26 @@
 <!--Allows user/site admin to write blog posts for the health and wellbeing page. As per System Requirements.-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Health & Wellbeing Form</title>
+</head>
+
+<body>
+
+<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+<script>tinymce.init({selector: 'textarea'});</script>
+<form action="createarticle" method="post">
+    <input type="text" name="articleName" placeholder="Article Name">
+    <textarea name="articleText"></textarea>
+    <input type="submit">
+</form>
+
+</body>
+</html>
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -6,3 +28,4 @@
  * Date: 07/11/2016
  * Time: 12:24
  */
+
