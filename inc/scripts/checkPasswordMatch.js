70080b7dddf499ce07d790a3f442db6026a48c62
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-25 11:40:37 +0000

    fixed minor typo2

--- inc/scripts/checkPasswordMatch.js
+++ inc/scripts/checkPasswordMatch.js
@@ -2,7 +2,7 @@
  * Created by hype_ on 25/11/2016.
  */
 $(document).ready(function () {
-    $("#secondPassword").onkeyup(checkPasswordMatch);
+    $("#secondPassword").keyup(checkPasswordMatch);
 });
 
 function checkPasswordMatch() {
