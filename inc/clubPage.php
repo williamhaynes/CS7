f8234a1eae8cabe567192a7de3339a11de2503dc
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-28 18:04:18 +0000

    Trying to create an individual club page by pulling params from url

--- inc/clubPage.php
+++ inc/clubPage.php
@@ -1,29 +1,30 @@
 <!--Will display all the stored information on each club as single web pages-->
 
+
 <?php
-include ("scripts/header.php");
-
-echo "
-    <main>
-    <p>Welcome to SPECIFIC!! Clubs and Societies Page</p>
-    </main>
-    ";
-
-
-$selectedClub = $_POST['selectedClub'];
-
-//Takes all database information from the Club Table for a chosen club.
-$sql_query = "SELECT * FROM " . $selectedClub . ";";
-
-//Process the query
-$result = $db->query($sql_query);
-
-// Iterate through the result and present data (This needs to be tidied into a displayable format, but does grab all available data)
-while ($row = $result->fetch_array()) {
-    echo "<p>" . $row['clubName'] . "</p>";
-    echo "<p>" . $row['clubDescription'] . "</p>";
-    echo "<p>" . $row['contactInformation'] . "</p>";
-}
-
-include ("scripts/footer.php");
+    include ("scripts/dbconnect.php");
+    include ("scripts/header.php");
+    $clubID = $params['clubID'];
+    echo "<main>";
+        //Takes all database information from the Club Table for a chosen club.
+        $sql = "SELECT * FROM club where clubID = '$clubID'";
+        //Process the query
+        $result = $db->query($sql);
+        // Iterate through the result and present data (This needs to be tidied into a displayable format, but does grab all available data)
+        while($row = $result->fetch_array())
+        {
+            $clubID = $row['clubID'];
+            $clubName = $row['clubName'];
+            $clubDescription = $row['clubDescription'];
+            $contactInformation = $row['contactInformation'];
+            $adminID = $row['adminID'];
+            echo "
+            <atricle>
+                 <h2>{$clubName}</h2>
+                 <p>{$clubDescription}</p>
+                 <p>{$contactInformation}</p>
+             </atricle>";
+        }
+        echo "</main>";
+    include ("scripts/footer.php");
 ?>
