5e71c93cf148e775ea1f5daacddfdc9e90c57bec
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-28 13:41:10 +0000

    sessionid debugging

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -23,7 +23,7 @@ session_start();
                     echo "<li><a href='createarticle'>Create Article</a></li>";
                     echo "<li><a href='logout'>Logout</a></li>";
                 } else {
-                    echo "<li><a href='login'>Login</a></li>";
+                    echo "<li><a href='loginPage'>Login</a></li>";
                 }
                 ?>
             </ul>
