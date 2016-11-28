1ff739c1dbf346f164d83e5cc077284b15e69506
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-28 11:14:52 +0000

    removed comments around controls for session

--- inc/scripts/checkLogin.php
+++ inc/scripts/checkLogin.php
@@ -46,9 +46,10 @@ else{           //if query returned a result
         echo "<p>" . $row['levelCode'] . "</p>";
     }*/
     echo "<p>Welcome " . $userName . "</p>";
+
     session_start();
     $_SESSION["username"] = $username;
-    //header("location:..\healthAndWellbeing.php");
+    header("location: ../clubsAndSocietiesPage.php");
 }
 
 
