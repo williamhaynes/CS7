654a31a2fb0f096cbb1c2aed6db2dbed8e4d6d3f
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-29 23:03:40 +0000

    usersAdmin troubleshoot

--- inc/AdminPages/usersAdminPage.php
+++ inc/AdminPages/usersAdminPage.php
@@ -16,23 +16,20 @@ include("../scripts/header.php");
         </tr>
         <?php
         //Takes all database information from the Users Table.
-
         $sql_query = "SELECT * FROM User;";
-
         //Process the query
         $result = $db->query($sql_query);
-
         // Iterate through the result and present data (This needs to be tidied into a displayable format, but does grab all available data)
         while($row = $result->fetch_array()){
             echo "<tr><th>" . $row['userName'] . "</th>";
             echo "<th>" . $row['emailAddress'] . "</th>";
             echo "<th>" . $row['displayName'] . "</th>";
-            echo "<th>" . $row['levelCode'] . "</th>";
-            echo "</tr>";
+            echo "<th>" . $row['levelCode'] . "</th></tr>";
         }
         ?>
     </table>
-    </main>
+    <p>Got to between table and main</p>
+</main>
 <?php
 echo "<p>Got to end of file before footer</p>";
 include ("../scripts/footer.php");
