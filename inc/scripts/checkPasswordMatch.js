4605e0063fe484dc02235250e4b21b3de2dcd9ec
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-29 11:51:00 +0000

    checkPassword doesn't work - returning to original state

--- inc/scripts/checkPasswordMatch.js
+++ inc/scripts/checkPasswordMatch.js
@@ -6,6 +6,9 @@
 /*DOESN'T WORK ATM*/
 
 //I understand what I want this to do but it doesn't work, checking the console it doesn't seem to run the event listener
+$(document).ready(function () {
+    $("#secondPassword").keyup(checkPasswordMatch());
+});
 
 function checkPasswordMatch() {
     var password = $("#firstPassword").val();
