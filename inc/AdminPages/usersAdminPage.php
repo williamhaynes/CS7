1b1d079d8eb1432c8936e2ce9fadbd28b27a30e2
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-29 18:27:39 +0000

    troubleshooting issue with dbconnect in usersAdminPAge

--- inc/AdminPages/usersAdminPage.php
+++ inc/AdminPages/usersAdminPage.php
@@ -2,18 +2,18 @@
 change their access controls.-->
 <?php
 include("../scripts/header.php");
-echo "
+?>
 <main>
-<h2>Users Admin Page</h2>
-<p>Below is a list of all Members</p>
-<table>
-    <tr>
-        <th>Username</th>
-        <th>Email Address</th>
-        <th>Display Name</th>
-        <th>Level Code</th>
-    </tr>";
-
+    <h2>Users Admin Page</h2>
+    <p>Below is a list of all Members</p>
+        <table>
+        <tr>
+            <th>Username</th>
+            <th>Email Address</th>
+            <th>Display Name</th>
+            <th>Level Code</th>
+        </tr>
+<?php
 //Takes all database information from the Users Table.
 include("../scripts/dbconnect.php");
 $sql_query = "SELECT * FROM User;";
