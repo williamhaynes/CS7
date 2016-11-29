6f0bd007b9720ac7d37bc911f82f3e0b10860b6e
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-29 11:07:15 +0000

    header - modified urls to try and fix

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -19,9 +19,9 @@ session_start();
                 <li id="mapPage"><a href="../mapPage">Maps</a></li>
                  <?php
                 if (isset($_SESSION['username'])) {
-                    echo "<li><a href='logoutPage'>Logout</a></li>";
+                    echo "<li><a href='../logoutPage'>Logout</a></li>";
                 } else {
-                    echo "<li><a href='loginPage'>Login</a></li>";
+                    echo "<li><a href='../loginPage'>Login</a></li>";
                 }
                 ?>
             </ul>
