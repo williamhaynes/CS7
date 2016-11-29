e9c451f5f8dde2a06a9d14b22c8e418a21f6904b
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-29 13:42:45 +0000

    modified header to allow access to usersAdminPage modified usersAdminPage to access user Date (hopefully)

--- inc/AdminPages/usersAdminPage.php
+++ inc/AdminPages/usersAdminPage.php
@@ -2,9 +2,28 @@
 change their access controls.-->
 
 <?php
-/**
- * Created by PhpStorm.
- * User: hype_
- * Date: 07/11/2016
- * Time: 12:10
- */
+include ("../scripts/dbconnect.php");
+include ("../scripts/header.php");
+echo "
+<main>
+<h2>Users Admin Page</h2>
+<p>Below is a list of all Members</p>
+<ul>";
+
+//Takes all database information from the Users Table.
+$sql_query = "SELECT * FROM User;";
+
+//Process the query
+$result = $db->query($sql_query);
+
+// Iterate through the result and present data (This needs to be tidied into a displayable format, but does grab all available data)
+while($row = $result->fetch_array()){
+    $user = $row['userName'];
+    $level = $row['levelCode'];
+    echo "<li><a href='clubPage/{user}'> $level </a></li>";
+}
+
+echo "</main>";
+include ("../scripts/footer.php");
+
+?>
