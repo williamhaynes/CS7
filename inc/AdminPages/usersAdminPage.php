3309c6b495e39fcf7b90087e3ab11b41403e8174
williamhaynes <w.haynes@rgu.ac.uk>
2016-11-29 17:39:50 +0000

    added table output of user data

--- inc/AdminPages/usersAdminPage.php
+++ inc/AdminPages/usersAdminPage.php
@@ -4,11 +4,18 @@ change their access controls.-->
 <?php
 include ("../scripts/dbconnect.php");
 include ("../scripts/header.php");
+
 echo "
 <main>
 <h2>Users Admin Page</h2>
 <p>Below is a list of all Members</p>
-<ul>";
+<table>
+    <tr>
+        <th>Username</th>
+        <th>Email Address</th>
+        <th>Display Name</th>
+        <th>Level Code</th>
+    </tr>";
 
 //Takes all database information from the Users Table.
 $sql_query = "SELECT * FROM User;";
@@ -18,12 +25,17 @@ $result = $db->query($sql_query);
 
 // Iterate through the result and present data (This needs to be tidied into a displayable format, but does grab all available data)
 while($row = $result->fetch_array()){
-    $user = $row['userName'];
-    $level = $row['levelCode'];
-    echo "<li><a href='clubPage/{user}'> $level </a></li>";
+
+    echo "<tr>
+                <th>" . $row['userName'] . "</th>
+                <th>" . $row['emailAddress'] . "</th>
+                <th>" . $row['displayName'] . "</th>
+                <th>" . $row['levelCode'] . "</th>
+          </tr>";
 }
 
-echo "</main>";
+echo "</table>
+</main>";
 include ("../scripts/footer.php");
 
 ?>
