8287c0cf80d6c74863790216494f747410556467
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-30 12:13:40 +0000

    Table name was wrong

--- inc/Forms/createHealthAndWellbeingForm.php
+++ inc/Forms/createHealthAndWellbeingForm.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['username']))
         $articleName = $_POST["articleName"];
         $articleText = $_POST["articleText"];
 
-        $sql = "INSERT INTO Health News (title, content, verified) VALUES ('" . $articleName . "', '" . $articleText . "', NULL )";
+        $sql = "INSERT INTO healthnews (title, content, verified) VALUES ('" . $articleName . "', '" . $articleText . "', 0 )";
         if (mysqli_query($db, $sql)) {
             header("location:../healthAndWellbeingPage");
         } else {
