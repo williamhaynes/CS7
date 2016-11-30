5c300b768021d6210f9ed75923b5a4c719afdac3
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-30 13:33:39 +0000

    Taking the .. out so href go to right place

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -16,20 +16,20 @@ session_start();
         <nav id="navBar">
             <ul id="header">
                 <li id="logo"><img id="logoIMG" src="https://pmcdeadline2.files.wordpress.com/2016/07/logo-tv-logo.png" alt="Logo"></li>
-                <li id="healthAndWellbeing"><a href="../healthAndWellbeingPage">Health & Wellbeing</a></li>
-                <li id="clubsAndSocieties"><a href="../clubsAndSocietiesPage">Clubs & Societies</a></li>
-                <li id="mapPage"><a href="../mapPage">Maps</a></li>
+                <li id="healthAndWellbeing"><a href="/healthAndWellbeingPage">Health & Wellbeing</a></li>
+                <li id="clubsAndSocieties"><a href="/clubsAndSocietiesPage">Clubs & Societies</a></li>
+                <li id="mapPage"><a href="/mapPage">Maps</a></li>
                  <?php
                 if (isset($_SESSION['username'])) {
-                    echo "<li><a href='../logoutPage'>Logout</a></li>";
-                    echo "<li><a href='../userDetailsPage'>My Account</a></li>";
+                    echo "<li><a href='/logoutPage'>Logout</a></li>";
+                    echo "<li><a href='/userDetailsPage'>My Account</a></li>";
                     if($_SESSION['accessLevel'] == 31){
-                        echo "<li><a href='../userDetailsPage'>Club Admin Page</a></li>";
-                        echo "<li><a href='../usersAdminPage'>User Admin Page</a></li>";
+                        echo "<li><a href='/userDetailsPage'>Club Admin Page</a></li>";
+                        echo "<li><a href='/usersAdminPage'>User Admin Page</a></li>";
                     }
                 } else {
-                    echo "<li><a href='../loginPage'>Login</a></li>";
-                    echo "<li><a href='../registerPage'>Register</a></li>";
+                    echo "<li><a href='/loginPage'>Login</a></li>";
+                    echo "<li><a href='/registerPage'>Register</a></li>";
                 }
                 ?>
             </ul>
