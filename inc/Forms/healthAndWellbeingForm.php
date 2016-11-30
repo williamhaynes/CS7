1f27d47f042692b0c3c69aaeb042e10a561c8923
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-30 14:05:01 +0000

    Copying other page format

--- inc/Forms/healthAndWellbeingForm.php
+++ inc/Forms/healthAndWellbeingForm.php
@@ -4,18 +4,16 @@ session_start();
 if (isset($_SESSION['username']))
 {
     include (__DIR__ . "/../scripts/dbconnect.php");
-    $itemID = $params['itemID'];
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         include(__DIR__."/../scripts/header.php");
-
-
+        $itemID = $params['itemID'];
+        $_SESSION['itemID'] = $itemID;
             //Takes all database information from the healthnews for a chosen article.
             $sql = "SELECT * FROM healthnews where itemID = '$itemID'";
             //Process the query
             $result = $db->query($sql);
             // Iterate through the result and present data (This needs to be tidied into a displayable format, but does grab all available data)
             while($row = $result->fetch_array()) {
-                $_SESSION['itemID'] = $itemID;
                 $title = $row['title'];
                 $_SESSION['title'] = $title;
                 $content = $row['content'];
@@ -27,7 +25,7 @@ if (isset($_SESSION['username']))
             <main>
                 <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
                 <script>tinymce.init({selector: 'textarea'});</script>
-                <form action='/Forms/healthAndWellbeingForm/<?php print $_SESSION["itemID"];?>' method="post">
+                <form action='/Forms/healthAndWellbeingForm' method="post">
                     <p>Article Name: </p><input type="text" name="title" value="<?php print $_SESSION["title"];?>" placeholder="Article Name">
                     <p>Content: </p> <textarea name="content"> <?php print $_SESSION["content"];?> Text itemid: <?php print $_SESSION["itemID"];?> </textarea>
                     <p>Verified: </p> <input type="text" name="verified" value="<?php print $_SESSION["verified"];?>" placeholder="verified">
