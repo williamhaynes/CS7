3e7c24104f624f36d32da86f577f2c4b3816a1a5
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-30 23:22:39 +0000

    Unsetting user data when logout

--- inc/logoutPage.php
+++ inc/logoutPage.php
@@ -1,8 +1,12 @@
 <?
 session_start();
 if (isset($_SESSION['username'])) {
+    unset($_SESSION['username']);
+    unset($_SESSION['accessLevel']);
+    unset($_SESSION['userID']);
+
     //Unset all session
-    session_unset();
+    //session_unset();
 }
 
 header("location:../clubsAndSocietiesPage");
