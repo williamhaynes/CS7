654bebf38ab3b5a0283cf0770dc69b8739a40634
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-30 11:36:53 +0000

    If no adminID set it to NULL

--- inc/Forms/createClubForm.php
+++ inc/Forms/createClubForm.php
@@ -28,7 +28,7 @@ if (isset($_SESSION['username']))
 
         //If adminID is blank set it to NULL
         if ($adminID == ""){
-            $adminID = "0";
+            $adminID = 'NULL';
         }
 
         $sql = "INSERT INTO Club (clubName, clubDescription, contactInformation, adminID)
