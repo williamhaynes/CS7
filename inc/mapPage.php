3866c8c288d0d9b82fda8993d270091562ecb514
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-01 23:16:18 +0000

    Take away onload and run javascript

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -9,13 +9,13 @@ echo "
         <title>Map</title>
         <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDEU8Mfp0WPoXcqq8gJdbUTogp-6yDzXcE' type='text/JavaScript'></script> 
         <script type='text/JavaScript'>
-            function load() {
+
               var map = new google.maps.Map(document.getElementById('map'), {
                 center: new google.maps.LatLng(57.063408, -2.1455154),
                 zoom: 13,
                 mapTypeId: 'roadmap'
               });
-            }
+
             // This event listener calls addMarker() when the map is clicked.
               google.maps.event.addListener(map, 'click', function(e) {
                 placeMarker(e.latLng, map);
@@ -32,7 +32,7 @@ echo "
 
     </head>
     <main>
-        <body onload='load()'>
+        <body>
              <div id='map' style='width: 1000px; height: 600px'></div>
         </body>
     </main>
