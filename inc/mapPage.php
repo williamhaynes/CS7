3600590ab42224cb56c13c50c285690e8b1fed97
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-01 21:59:04 +0000

    Adding a google map to map page

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -11,7 +11,7 @@ echo "
         <script type=\"text/JavaScript\">
             function load() {
               var map = new google.maps.Map(document.getElementById('map'), {
-                center: new google.maps.LatLng(47.6145, -122.3418),
+                center: new google.maps.LatLng(57.063408, -2.1455154),
                 zoom: 13,
                 mapTypeId: 'roadmap'
               });
