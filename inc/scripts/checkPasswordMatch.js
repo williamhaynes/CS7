dca1f45e60e37be8562e5a2c54e859af9609d6c1
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-01 19:26:12 +0000

    updated css and class html for column row hovers

--- inc/scripts/checkPasswordMatch.js
+++ inc/scripts/checkPasswordMatch.js
@@ -7,16 +7,16 @@
 
 //I understand what I want this to do but it doesn't work, checking the console it doesn't seem to run the event listener
 $(document).ready(function () {
-    $("#secondPassword").keyup(checkPasswordMatch());
+    $('#secondPassword').keyup(checkPasswordMatch());
 });
 
 function checkPasswordMatch() {
-    var password = $("#firstPassword").val();
-    var confirmPassword = $("#secondPassword").val();
+    var password = $('#firstPassword').val();
+    var confirmPassword = $('#secondPassword').val();
 
-    if (password != confirmPassword)
-        $("#body").html("Passwords do not match!");
-    else
-        $("#body").html("Passwords match.");
+    if (password != confirmPassword){
+        $('#body').html("Passwords do not match!");}
+    else{
+        $('#body').html("Passwords match.");}
 }
 
