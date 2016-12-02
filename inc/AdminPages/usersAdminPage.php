6d00a9aa1b1f8cdd97932e04b06d7522742c3c6b
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-02 16:11:06 +0000

    adding ability to select user

--- inc/AdminPages/usersAdminPage.php
+++ inc/AdminPages/usersAdminPage.php
@@ -103,7 +103,8 @@ if($_SESSION['accessLevel'] == '31') {
             $result = $db->query($sql_query);
             // Iterate through the result and present data (This needs to be tidied into a displayable format, but does grab all available data)
             while ($row = $result->fetch_array()) {
-                echo "<tr class='hoverableRowsAndColumns'><td class='hoverableSpecificRowAndColumn'>" . $row['userName'] . "</td>";
+                $userID = $row['userID'];
+                echo "<tr class='hoverableRowsAndColumns' onclick=\"location.href='" . "userDetailsPage/{$userID}'\"><td class='hoverableSpecificRowAndColumn'>" . $row['userName'] . "</td>";
                 echo "<td class='hoverableSpecificRowAndColumn'>" . $row['emailAddress'] . "</td>";
                 echo "<td class='hoverableSpecificRowAndColumn'>" . $row['displayName'] . "</td>";
                 echo "<td class='hoverableSpecificRowAndColumn'>" . translateLevelCode($row['levelCode']) . "</td>";
