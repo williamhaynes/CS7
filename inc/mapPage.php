b9ffd8bb66904373d7cbe881c40753aa44368cdb
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-02 14:37:38 +0000

    Putting javascript inside php

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -4,7 +4,7 @@ System Requirements.-->
 <?php
 include ("scripts/header.php");
 
-echo "
+?>
     <head>
         <title>Map</title>
         <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDEU8Mfp0WPoXcqq8gJdbUTogp-6yDzXcE' type='text/JavaScript'></script> 
@@ -15,6 +15,35 @@ echo "
                 zoom: 13,
                 mapTypeId: 'roadmap'
               });
+
+                <?
+                    $sql_query = "SELECT * FROM Location;";
+
+                    //Once all information has been pulled process the SQL query
+                    $result = $db->query($sql_query);
+
+
+                    //Iterate through and sort out all the data
+                     while($row = $result->fetch_array()){
+                         $locationID = $row['locationID'];
+                         $name = $row['name'];
+                         $address = $row['address'];
+                         $lat = $row['lat'];
+                         $lng = $row['lng'];
+                         $typeID = $row['typeID'];
+                         echo "
+                                var marker = new google.maps.LatLng($lat, $lng);
+                                var marker = new google.maps.Marker({
+                                position: portlethenLatLng,
+                                    map: map,
+                                    title: '$name',
+                                    draggable: false
+                                });
+
+                         ";
+                     }
+                 ?>
+
             }
         </script>
         <a href='mapForm'>Link to Map Form</a>
@@ -24,7 +53,11 @@ echo "
              <div id='map' style='width: 1000px; height: 600px'></div>
         </body>
     </main>
-    ";
+
+
+<?
 
 include ("scripts/footer.php");
 ?>
+
+
