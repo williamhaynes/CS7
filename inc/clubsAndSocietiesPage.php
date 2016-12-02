145df6f12577bd819c5b395bfeddd7fe80cb4412
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-02 11:49:16 +0000

    adding filtering options

--- inc/clubsAndSocietiesPage.php
+++ inc/clubsAndSocietiesPage.php
@@ -46,7 +46,7 @@ if ($_SESSION['accessLevel']==21||$_SESSION['accessLevel']==31){
 
             // Loop through all table rows, and hide those who don't match the search query
             for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[0];
+                td = tr[i].getElementsByTagName("td")[section];
                 if (td) {
                     if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
                         tr[i].style.display = "";
