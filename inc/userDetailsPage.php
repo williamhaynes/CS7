339842e24553c998b970acccdcc822dacc23d9f2
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-02 18:01:05 +0000

    adding more user friendly modification of user levelcode to site administrator

--- inc/userDetailsPage.php
+++ inc/userDetailsPage.php
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             if($_SESSION['accessLevel'] == 31) {    //If Site Adminstrator give extra controls.
                 //Get existing value
                 $currentLevelCode = $row['levelCode'];
-                echo "<p>". $currentLevelCode ."</p>"
+                echo "<p>". $currentLevelCode ."</p>";
                 $contributorValue = "";
                 $nKPAGValue= "";
                 $clubAdminValue = "";
