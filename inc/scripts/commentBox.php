c93a181473b1fe3e5338ad2ad7fed335fd4f0d3f
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-04 19:03:41 +0000

    Adding a seperate file to upload to database

--- inc/scripts/commentBox.php
+++ inc/scripts/commentBox.php
@@ -1,9 +1,12 @@
+<!-- This is the comment box where signed in users can comment -->
+
 <?php
 session_start();
 include(__DIR__ . "/../scripts/dbconnect.php");
 if (isset($_SESSION['userID'])) {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         ?>
+
         <p>Tell us what you think!</p>
         <form action='./commentToDatabase' method="post">
             <textarea name="comment" id="comment" placeholder="Say something!"></textarea>
