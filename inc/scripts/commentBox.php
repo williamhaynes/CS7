9cdb4a4a093629601ec3be309629bfebf91df4c4
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-04 18:12:24 +0000

    Displaying the username of person logged in

--- inc/scripts/commentBox.php
+++ inc/scripts/commentBox.php
@@ -1,5 +1,20 @@
-<p>Tell us what you think!</p>
-<form method="post">
-    <textarea name="comment" id="comment" placeholder="Say something!"></textarea>
-    <p><input type="submit" value='Submit Comment'></p>
-</form>
+<?php
+session_start();
+include(__DIR__ . "/../scripts/dbconnect.php");
+if (isset($_SESSION['username'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $username = $_SESSION['username'];
+        ?>
+        <p>Tell us what you think!</p>
+        <form method="post">
+            <textarea name="comment" id="comment" placeholder="Say something!"></textarea>
+            <p><?$username?></p>
+            <p><input type="submit" value='Submit Comment'></p>
+        </form>
+        <?
+    }elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+}else{
+    //If not logged in don't show comment box
+} ?>
