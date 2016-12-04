79307226cf1accf33d679946d988eb16a96584fc
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-04 18:02:04 +0000

    Added more to the commentBox

--- inc/scripts/commentBox.php
+++ inc/scripts/commentBox.php
@@ -1,5 +1,5 @@
 <p>Tell us what you think!</p>
 <form method="post">
-    <p>Comment: </p><textarea name="comment" id="comment" placeholder="Say something!"></textarea>
+    <textarea name="comment" id="comment" placeholder="Say something!"></textarea>
     <p><input type="submit" value='Submit Comment'></p>
 </form>
