530c3164b6f130e3e915de5b688ab3309517d72c
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-05 12:29:35 +0000

    Fixing commentbox

--- inc/scripts/commentToDatabase.php
+++ inc/scripts/commentToDatabase.php
@@ -1,9 +1,10 @@
 <?php
+session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     include(__DIR__ . "/../scripts/dbconnect.php");
     $comment = $_POST["comment"];
     $userID = $_SESSION["userID"];
-    echo "<script>alert($comment); alert($userID); </script>";
+    echo "<script> alert($userID); </script>";
     $sql = "INSERT INTO comment (comment,userID) VALUES ('" . $comment . "', '" . $userID . "')";
     if (mysqli_query($db, $sql)) {
         echo "<p>Comment Successful!</p>";
