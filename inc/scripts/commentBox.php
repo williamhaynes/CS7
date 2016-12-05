651d64c60bca025edad83f620c82b4b55b1dee9c
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-05 12:41:41 +0000

    Pass current URL through form

--- inc/scripts/commentBox.php
+++ inc/scripts/commentBox.php
@@ -10,13 +10,13 @@ if (isset($_SESSION['userID'])) {
         <p xmlns="http://www.w3.org/1999/html">Tell us what you think!</p>
         <form action='/../commentToDatabase' method="post">
             <textarea name="comment" id="comment" placeholder="Say something!"></textarea>
-            <input type="hidden" id='currentUrl' name="currentUrl">
+            <input type="hidden" id='currentUrl' name="currentUrl" value="">
             <p><input type="submit" value='Submit Comment'></p>
         </form>
         
         <script>
-            alert(document.URL);
-            document.getElementById('currentUrl').setAttribute(value, document.URL);
+            var url = document.URL;
+            document.getElementById('currentUrl').setAttribute('value', url);
         </script>
         <?
     }
