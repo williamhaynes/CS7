bf2b88848670572748d4e11f5fee395e4541e308
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-05 20:08:26 +0000

    troubleshoot

--- inc/scripts/calendar.php
+++ inc/scripts/calendar.php
@@ -28,6 +28,7 @@ include("dbconnect.php");
     while($row = $result->fetch_array()) {                           //Iterate through Query Results
         //if all clubs
         echo"<p>Club Name is".$row['clubName']."</p>";
+        echo"<p>Club Name is".$result."</p>";
         if ($currentURL == "/clubsAndSocietiesPage") {
             //Implement table row
             echo "<tr>";
