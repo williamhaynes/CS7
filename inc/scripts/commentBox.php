e52df9faf80ca02eec828785b176cdcf24b7d3c2
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-05 12:36:19 +0000

    Pass current URL through form

--- inc/scripts/commentBox.php
+++ inc/scripts/commentBox.php
@@ -7,11 +7,16 @@ if (isset($_SESSION['userID'])) {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         ?>
 
-        <p>Tell us what you think!</p>
+        <p xmlns="http://www.w3.org/1999/html">Tell us what you think!</p>
         <form action='/../commentToDatabase' method="post">
             <textarea name="comment" id="comment" placeholder="Say something!"></textarea>
+            <input type="hidden" id='currentUrl' name="currentUrl">
             <p><input type="submit" value='Submit Comment'></p>
         </form>
+        
+        <script>
+            document.getElementById('currentUrl').setAttribute(value, document.URL);
+        </script>
         <?
     }
 }else{
--- inc/scripts/commentToDatabase.php
+++ inc/scripts/commentToDatabase.php
@@ -3,6 +3,7 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     include(__DIR__ . "/../scripts/dbconnect.php");
     $comment = $_POST["comment"];
+    $comment = $_POST["currentUrl"];
     $userID = $_SESSION["userID"];
     echo "<script> alert($userID); </script>";
     $sql = "INSERT INTO comment (comment,userID) VALUES ('" . $comment . "', '" . $userID . "')";
