aa02dd219fe055b6419766f6b4ffff4b6df2e12d
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-05 12:35:34 +0000

    adding calendar

---
+++ inc/scripts/calendar.php
@@ -0,0 +1,33 @@
+<!-- PHP file to allow includeable calendar on relevant pages -->
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hype_
+ * Date: 05/12/2016
+ * Time: 11:58
+ */
+include("dbconnect.php");
+    session_start();                                                //Allow access to session cookies
+    $clubID = $_SESSION['clubID'];                                  //get required session cookie
+    $sql_query;                                                     //Initialize query
+    if(true){            //If page making call P&S page
+        $sql_query = "SELECT * FROM Club Calander;";                //Get all events
+    }
+    else {                                                          //Call by specific club
+        $sql_query = "SELECT * FROM Club Calendar WHERE = '". $clubID."';";     //Get specific Events
+    }
+
+    $result = $db->query($sql_query);                               //Process relevant query
+    while($row = $result->fetch_array()){                           //Iterate through Query Results
+        //if all clubs
+            //Echo event name
+            //Echo event date
+        //if specific club
+            //Echo event name
+            //Echo event date
+            //Echo event Description
+    }
+
+
+
+?>
