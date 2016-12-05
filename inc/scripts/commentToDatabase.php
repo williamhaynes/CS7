b0bb86e3529d1506ecbda27d27e6ae9b46eb4140
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-05 12:37:24 +0000

    Pass current URL through form

--- inc/scripts/commentToDatabase.php
+++ inc/scripts/commentToDatabase.php
@@ -3,7 +3,7 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     include(__DIR__ . "/../scripts/dbconnect.php");
     $comment = $_POST["comment"];
-    $comment = $_POST["currentUrl"];
+    $currentUrl = $_POST["currentUrl"];
     $userID = $_SESSION["userID"];
     echo "<script> alert($currentUrl); </script>";
     $sql = "INSERT INTO comment (comment,userID) VALUES ('" . $comment . "', '" . $userID . "')";
