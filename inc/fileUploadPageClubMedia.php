343094509f5e84e0f20d4154885a9bd979c7e68d
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-05 15:40:15 +0000

    Updating fileUploadPage

--- inc/fileUploadPageClubMedia.php
+++ inc/fileUploadPageClubMedia.php
@@ -1,30 +1,43 @@
-<?php
-include_once 'dbconfig.php';
-?>
+<!-- This is the upload button where signed in users can upload -->
 
-            <form action="/../uploadClubMedia" method="post" enctype="multipart/form-data">
-                <input type="file" name="file" />
-                <input type="hidden" name="userID" value=<?php print $userID;?> />
-                <button type="submit" name="btn-upload">upload</button>
-            </form>
-            <br /><br />
-            <?php
-            if(isset($_GET['success']))
-            {
-                ?>
-                <label>File Uploaded Successfully...  <a href="/../viewUploads">click here to view file.</a></label>
-                <?php
-            }
-            else if(isset($_GET['fail']))
-            {
-                ?>
-                <label>Problem While File Uploading !</label>
-                <?php
-            }
-            else
-            {
-                ?>
-                <label>Try to upload any files(PDF, DOC, EXE, VIDEO, MP3, ZIP,etc...)</label>
-                <?php
-            }
+<?php
+session_start();
+include(__DIR__ . "/../scripts/dbconnect.php");
+if (isset($_SESSION['userID'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $afterSlashUrl = $_SERVER['REQUEST_URI'];
+        $userID = $_SESSION['userID'];
+        ?>
+
+        <form action="/../uploadClubMedia" method="post" enctype="multipart/form-data">
+            <input type="file" name="file" />
+            <input type="hidden" name="userID" value=<?php print $userID;?> />
+            <button type="submit" name="btn-upload">upload</button>
+        </form>
+        <br /><br />
+        <?php
+        if(isset($_GET['success']))
+        {
             ?>
+            <label>File Uploaded Successfully...  <a href="/../viewUploads">click here to view file.</a></label>
+            <?php
+        }
+        else if(isset($_GET['fail']))
+        {
+            ?>
+            <label>Problem While File Uploading !</label>
+            <?php
+        }
+        else
+        {
+            ?>
+            <label>Try to upload any files(PDF, DOC, EXE, VIDEO, MP3, ZIP,etc...)</label>
+            <?php
+        }
+        ?>
+        <?
+    }
+}else{
+    //If not logged in don't show comment box
+    echo"<p>Login to comment</p>";
+} ?>
