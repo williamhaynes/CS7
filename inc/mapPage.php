9401c40cdcc1f5f88baaa0c4836eb514aea76418
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-06 23:17:26 +0000

    Trying to get route information from database

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -109,7 +109,7 @@ include ("scripts/dbconnect.php");
                         "address": "<?php print $row['address'];?>",
                         "description": "<?php print $row['description'];?>",
                     <?
-                        $sql_querySpecificRoute = 'SELECT * FROM route WHERE locationID = $row[\'locationID\']';
+                        $sql_querySpecificRoute = 'SELECT * FROM route WHERE locationID = $row[locationID]';
                         echo "$sql_querySpecificRoute";
                         $resultSpecificRoute = $db->query($sql_querySpecificRoute);
                         while ($rowRoute = $resultSpecificRoute->fetch_array()) {
