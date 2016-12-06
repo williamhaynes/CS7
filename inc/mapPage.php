77701eb759f32c03360dd0a0936d3ac2b35d70e6
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-06 13:15:31 +0000

    Updating mapPage to display googlemaps JSON call

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -78,7 +78,7 @@ include(__DIR__ . "/../scripts/dbconnect.php");
     // Loop through the results array and place a marker for each
     // set of coordinates.
     window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
+        for (var i = 0; i < results.markers.length; i++) {
             var coords = results.features[i].geometry.coordinates;
             var latLng = new google.maps.LatLng(coords[1],coords[0]);
             var marker = new google.maps.Marker({
