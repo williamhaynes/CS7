5d5a9d25884e0152aee42f1864ab4f6e87f19eed
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-08 16:04:23 +0000

    Trying to create a route

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -442,8 +442,8 @@ include ("scripts/dbconnect.php");
     }
 
     function calcRoute() {
-        alert(document.getElementById('firstPointLatLng').value);
-        var start =  new google.maps.LatLng(document.getElementById('firstPointLatLng').value);
+        var startCoord = document.getElementById('firstPointLatLng').value;
+        var start =  new google.maps.LatLng(startCoord);
         var end =  new google.maps.LatLng(document.getElementById('secondPointLatLng').value);
         var bounds = new google.maps.LatLngBounds();
         bounds.extend(start);
