24dda298af112df02d4253628990ce3b0da037ab
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-08 09:09:48 +0000

    add event to calendar

--- inc/Forms/eventsForm.php
+++ inc/Forms/eventsForm.php
@@ -7,28 +7,25 @@ if (isset($_SESSION['username'])) {
         include(__DIR__ . "/../scripts/dbconnect.php");
         ?>
         <main>
-            <body>
-            <p>Oh my my my my my my</p>
+            <h2>Add Event to Calendar</h2>
             <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
             <script>tinymce.init({selector: 'textarea'});</script>
             <form action="eventsForm.php" method="post">
-                <input type="text" placeholder="Event Name" name="eventName">
-                <input type="text" placeholder="Start Date" name="eventStartDate">
-                <input type="text" placeholder="End Date" name="eventEndDate">
+                <p>Event Name:</p>
+                <p><input type="text" placeholder="Event Name" name="eventName"></p>
+                <p>Event Start Date</p>
+                <p><input type="text" placeholder="Start Date" name="eventStartDate"></p>
+                <p>Event Description</p>
+                <p><input type="text" placeholder="End Date" name="eventEndDate"></p>
                 <textarea name="eventDescription"></textarea>
                 <input type="submit" value='Create Event'>
             </form>
-
-            </body>
+        </main>
         <?
         include(__DIR__ . "/../scripts/footer.php");
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        /*
-        //include database
-        include ("../scripts/dbconnect.php");
-
         //variables
-        $clubID = $params['clubID'];
+        $clubID = $_SESSION['clubID'];
         $eventName = $_POST["eventName"];
         $eventStartDate = $_POST["eventStartDate"];
         $eventEndDate = $_POST["eventEndDate"];
@@ -39,7 +36,7 @@ if (isset($_SESSION['username'])) {
             header("location:/clubPage/$clubID");
         } else {
             echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
-        }*/
+        }
     }
     //If not signed in send them to the login screen
 }else {
