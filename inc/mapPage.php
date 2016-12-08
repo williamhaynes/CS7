f8cee511eb5e69755b1020144802518a2675993b
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-08 22:50:52 +0000

    Moving some css

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -7,19 +7,6 @@ include ("scripts/dbconnect.php");
 ?>
 <head>
     <link rel="stylesheet" type="text/css" href="/style/mapStyle.css">
-    <style>
-        /* Always set the map height explicitly to define the size of the div
-         * element that contains the map. */
-        #map {
-            height: 100%;
-        }
-        /* Optional: Makes the sample page fill the window. */
-        html, body {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-        }
-    </style>
 </head>
 <body>
 
