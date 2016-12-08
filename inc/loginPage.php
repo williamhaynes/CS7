d4f8a2adb8a3222a29662cacdb1a6604397363b3
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-08 09:43:05 +0000

    css updates and html changes to accommodate them for registerPage

--- inc/loginPage.php
+++ inc/loginPage.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     ?>
     <main>
         <div id="divForLoginPage">
-            <h2 id="loginPageHeaderText">User Login</h2>
+            <h2 class="pageHeaderText">User Login</h2>
             <form action="loginPage" method="post">
                 <input type="text" placeholder="User Name or Email Address" name="username" class="loginPageTextBox">
                 <p><input type="password" placeholder="Password" name="password" class="loginPageTextBox"></p>
