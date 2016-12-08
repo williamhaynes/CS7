06ece7f0cdef49708f742acebfe223d20428ca76
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-08 22:56:54 +0000

    Changing size of map

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -15,7 +15,7 @@ include ("scripts/dbconnect.php");
         }
         /* Optional: Makes the sample page fill the window. */
         html, body {
-            height: 10%;
+            height: 90%;
             margin: 0;
             padding: 0;
         }
