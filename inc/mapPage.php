05e255045d220daa2be44dec71fee7312c15b93b
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-08 21:04:26 +0000

    Making all markers allowed to route together

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -44,6 +44,35 @@ include ("scripts/dbconnect.php");
             center: portlethenLatLng
         });
 
+        var currentPosInfoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                currentPosInfoWindow.setPosition(pos);
+                currentPosInfoWindow.setContent('Location found.');
+                map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, currentPosInfoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, currentPosInfoWindow, map.getCenter());
+        }
+    }
+
+    function handleLocationError(browserHasGeolocation, currentPosInfoWindow, pos) {
+        currentPosInfoWindow.setPosition(pos);
+        currentPosInfoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+    }
+
         var icons = {
             landmark: {
                 name: 'Landmark',
