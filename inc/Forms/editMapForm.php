8b1dffbc7de3b53e45206be8e150eacfcfb1a6e5
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-09 18:25:09 +0000

    Trying to connect to db

--- inc/Forms/editMapForm.php
+++ inc/Forms/editMapForm.php
@@ -41,29 +41,18 @@ if (isset($_SESSION['username'])) {
             <p>Place Name: <input size='20' type='text' name='name' value="<?php print $row['name'];?>" placeholder='Place Name'></p>
             <p>Address: <input size='20' type='text' name='address' value="<?php print $row['address'];?>" placeholder='Address'></p>
             <p>Description: <textarea name="description" id="description"><?php print $row['description'];?></textarea></p>
-            <p>Latitude: <input size='20' type='text' id='latbox' name='lat' value='<?php print $row['lat'];?>'></p>
-            <p>Longitude: <input size='20' type='text' id='lngbox' name='lng' value='<?php print $row['lng'];?>'></p>
+            <p>Latitude: <input size='20' type='text' id='latbox' name='lat' value='<?php print $row['lat'];?>' readonly></p>
+            <p>Longitude: <input size='20' type='text' id='lngbox' name='lng' value='<?php print $row['lng'];?>' readonly></p>
+            <p>Type: <input size='20' type='text' id='type' readonly></p>
             <script>if (<?php print $row['typeID'];?>==4){
-                <?
-                $sql_queryRoute = "SELECT * FROM route WHERE locationID = '$locationID'";
-                $resultRoute = $db->query($sql_queryRoute);
-                while ($rowRoute = $resultRoute->fetch_array()) {?>
-                    <p>latlngString: <input size='20' type='text' id='latlngString' name='latlngString' value='<?php print $rowRoute['array'];?>'></p>
-                    <p>Type: Route</p>
-                <?}?>
+                document.getElementById('type').value = 'Route';
             }else if(<?php print $row['typeID'];?>==3){
-                    <?
-                    $sql_queryArea = "SELECT * FROM area WHERE locationID = '$locationID'";
-                    $resultArea = $db->query($sql_queryArea);
-                    while ($rowArea = $resultArea->fetch_array()) {?>
-                        <p>latlngString: <input size='20' type='text' id='latlngString' name='latlngString' value='<?php print $rowArea['array'];?>'></p>
-                        <p>Type: Area</p>
-                    <?}?>
-                }else if(<?php print $row['typeID'];?>==2){
-                    document.write("<p>Type: Viewpoint </p>");
-                }else{
-                    document.write("<p>Type: Landmark </p>");
-                }
+                document.getElementById('type').value = 'Area';
+            }else if(<?php print $row['typeID'];?>==2){
+                document.getElementById('type').value = 'Viewpoint';
+            }else{
+                document.getElementById('type').value = 'Landmark';
+            }
             </script>
             <p><input type='submit' value='Submit'></p>
             <?}?>
@@ -76,61 +65,13 @@ if (isset($_SESSION['username'])) {
         $name = $_POST["name"];
         $address = $_POST["address"];
         $description = $_POST["description"];
-        $lat = $_POST["lat"];
-        $lng = $_POST["lng"];
-        $typeID = $_POST["typeID"];
-        $latlngString = $_POST["latlngString"];
-        //IF TYPEID = ROUTE
-        if ($typeID==4){
-            $sql = "INSERT INTO location (name, address, description, lat, lng, typeID) VALUES ('" . $name . "', '" . $address . "', '" . $description . "', " . $lat . ", " . $lng . ", " . $typeID . ")";
-            if (mysqli_query($db, $sql)) {
-                $sqlGetLocationID = "SELECT locationID FROM location WHERE name ='" . $name . "' AND address = '" . $address ."'";
-                $result = $db->query($sqlGetLocationID);
-                while($row = $result->fetch_array()){
-                    $resultLocationID = $row['locationID'];
-                }
-                $sql2 = "INSERT INTO route (array, locationID) VALUES ( '" . $latlngString . "', '" . $resultLocationID . "')";
-                if (mysqli_query($db, $sql2)) {
-                    header("location:../mapPage");
-                }
-                else {
-                    echo "Error: " . $sql2 . "<br>Error Message:" . mysqli_error($db);
-                }
-            } else {
-                echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
-            }
-        }
-        //IF TYPEID = AREA
-        elseif($typeID==3){
-            $sql = "INSERT INTO location (name, address, description, lat, lng, typeID) VALUES ('" . $name . "', '" . $address . "', '" . $description . "', " . $lat . ", " . $lng . ", " . $typeID . ")";
-            if (mysqli_query($db, $sql)) {
-                $sqlGetLocationID = "SELECT locationID FROM location WHERE name ='" . $name . "' AND address = '" . $address ."'";
-                $result = $db->query($sqlGetLocationID);
-                while($row = $result->fetch_array()){
-                    $resultLocationID = $row['locationID'];
-                }
-                $sql2 = "INSERT INTO area (array, locationID) VALUES ( '" . $latlngString . "', '" . $resultLocationID . "')";
-                if (mysqli_query($db, $sql2)) {
-                    header("location:../mapPage");
-                }
-                else {
-                    echo "Error: " . $sql2 . "<br>Error Message:" . mysqli_error($db);
-                }
-            } else {
-                echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
-            }
-        }
-        //IF TYPEID = LANDMARK OR VIEWPOINT
-        else {
-            $sql = "INSERT INTO location (name, address, description, lat, lng, typeID) VALUES ('" . $name . "', '" . $address . "', '" . $description . "', " . $lat . ", " . $lng . ", " . $typeID . ")";
-            if (mysqli_query($db, $sql)) {
-                header("location:../mapPage");
-            } else {
-                echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
-            }
+        $sql = "INSERT INTO location (name, address, description) VALUES ('" . $name . "', '" . $address . "', '" . $description . "')";
+        if (mysqli_query($db, $sql)) {
+            header("location:../mapPage");
+        } else {
+            echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
         }
     }
-    //test
 } else {
     header("location:404");
 }
