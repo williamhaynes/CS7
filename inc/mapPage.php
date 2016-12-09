d9483f52fe7f0b4d9337600a42ee50ecf0ea02f4
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-09 22:33:48 +0000

    Updating mapPage to hide edit buttons

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -320,13 +320,13 @@ include("scripts/dbconnect.php");
                         this.description +
                         '<div class="iw-subTitle">Address</div>' +
                         this.address +
-                        '<div class="iw-edit" style="display: none"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
+                        '<div class="iw-edit" id="iw-edit" style="display: none"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
                         '</div>' +
                         '</div>' +
                         '</div>');
                     //$("<div class='iw-outsidecontent'></div>").wrap("<div class='iw-content'></div>");
                     <?if(isset($_SESSION['username'])==true){?>
-                    document.getElementsByClassName("iw-edit").style.display = "";
+                    document.getElementById("iw-edit").style.display = '';
                     <?}?>
                     infowindow.open(map, this);
                     //If click remove routes and areas
@@ -367,13 +367,13 @@ include("scripts/dbconnect.php");
                         this.description +
                         '<div class="iw-subTitle">Address</div>' +
                         this.address +
-                        '<div class="iw-edit" style="display: none"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
+                        '<div class="iw-edit" id="iw-edit" style="display: none"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
                         '</div>' +
                         '</div>' +
                         '</div>');
                     //$("<div class='iw-outsidecontent'></div>").wrap("<div class='iw-content'></div>");
                     <?if(isset($_SESSION['username'])==true){?>
-                    document.getElementsByClassName("iw-edit").style.display = "";
+                    document.getElementById("iw-edit").style.display = '';
                     <?}?>
                     infowindow.open(map, this);
                     var areaArray = this.array.split(',');
