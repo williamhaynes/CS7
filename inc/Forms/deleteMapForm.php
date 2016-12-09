25b65f8896d089e53caab924ba390281f2e36749
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-09 19:18:53 +0000

    Updating azure

--- inc/Forms/deleteMapForm.php
+++ inc/Forms/deleteMapForm.php
@@ -6,11 +6,11 @@ if($_SESSION['accessLevel']==11||$_SESSION['accessLevel']==31){
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         include("/../scripts/dbconnect.php");
         $locationID = $params['locationID'];
-        $sql = "DELETE FROM location WHERE locationID = $locationID";
-        if (mysqli_query($db, $sql)) {
+        $sqlDelete = "DELETE FROM location WHERE locationID = $locationID";
+        if (mysqli_query($db, $sqlDelete)) {
             header("location:../mapPage");
         } else {
-            echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
+            echo "Error: " . $sqlDelete . "<br>Error Message:" . mysqli_error($db);
         }
     }
 }
