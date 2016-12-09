b93d150e0e42e966230e5c64072e0b6e144fb390
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-09 18:58:07 +0000

    DELETE MARKER FROM TABLE LOCATION QUERY

--- inc/Forms/deleteMapForm.php
+++ inc/Forms/deleteMapForm.php
@@ -4,7 +4,12 @@ if (isset($_SESSION['username'])) {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         include("/../scripts/dbconnect.php");
         $locationID = $params['locationID'];
-
+        $sql = "DELETE FROM location WHERE locationID = $locationID";
+        if (mysqli_query($db, $sql)) {
+            header("location:../mapPage");
+        } else {
+            echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
+        }
     }
 }
 ?>
