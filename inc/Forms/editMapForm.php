a8c0e61d9f3dc4e30114c1e26f61d145da2ee633
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-09 17:58:58 +0000

    Trying to connect to db

--- inc/Forms/editMapForm.php
+++ inc/Forms/editMapForm.php
@@ -20,12 +20,13 @@ if (isset($_SESSION['username'])) {
                     map = new google.maps.Map(document.getElementById('map'), {center: portlethenLatLng, zoom: 13, mapTypeId: 'roadmap'});
                     <?//Takes all database information from the location TABLE.
                     $sql_queryLatLng = "'SELECT * FROM location WHERE locationID = '+$locationID";
-                    echo "<script> alert('Im here') </script>";
+                    echo "<script> alert($sql_queryLatLng) </script>";
                     $resultLatLng = $db->query($sql_queryLatLng);
-                    echo "<script> alert('Im there') </script>";
+                    echo "<script> alert($resultLatLng) </script>";
                     while ($rowLatLng = $resultLatLng->fetch_array()) {?>
-                    markerLatLng = new google.maps.LatLng(<?php print $rowLatLng['lat'];?>, <?php print $rowLatLng['lng'];?>);
-                    marker = new google.maps.Marker({position: markerLatLng, map: map, title: 'Marker', draggable: true});
+                    echo "<script> alert('Inside') </script>";
+                        markerLatLng = new google.maps.LatLng(<?php print $rowLatLng['lat'];?>, <?php print $rowLatLng['lng'];?>);
+                        marker = new google.maps.Marker({position: markerLatLng, map: map, title: 'Marker', draggable: true});
                     <?}?>
                 }
             </script>
