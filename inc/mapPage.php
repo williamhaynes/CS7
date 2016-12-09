fd149717089ef9fe7ef6556203c32f0ebc11e535
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-09 22:23:40 +0000

    Updating mapPage to hide edit buttons

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -275,13 +275,13 @@ include("scripts/dbconnect.php");
                         '<div class="iw-subTitle">Address</div>' +
                         this.address +
                         '<div class="iw-edit" style="display: none"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
-                        if(<? isset($_SESSION['username'])==true ?>){
-                            document.getElementsByClassName("iw-edit").style.display = "";
-                        }
                         '</div>' +
                         '</div>' +
                         '</div>');
                     //$("<div class='iw-outsidecontent'></div>").wrap("<div class='iw-content'></div>");
+                    if(<? isset($_SESSION['username'])==true ?>){
+                        document.getElementsByClassName("iw-edit").style.display = "";
+                    }
                     infowindow.open(map, this);
                     //If click remove routes and areas
                     if (arrayOfPolylines.length > 0) {
@@ -321,13 +321,13 @@ include("scripts/dbconnect.php");
                         '<div class="iw-subTitle">Address</div>' +
                         this.address +
                         '<div class="iw-edit" style="display: none"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
-                        if(<? isset($_SESSION['username'])==true ?>){
-                            document.getElementsByClassName("iw-edit").style.display = "";
-                        }
                         '</div>' +
                         '</div>' +
                         '</div>');
                     //$("<div class='iw-outsidecontent'></div>").wrap("<div class='iw-content'></div>");
+                    if(<? isset($_SESSION['username'])==true ?>){
+                        document.getElementsByClassName("iw-edit").style.display = "";
+                    }
                     infowindow.open(map, this);
                     //If click remove routes and areas
                     if (arrayOfPolylines.length > 0) {
@@ -367,11 +367,14 @@ include("scripts/dbconnect.php");
                         this.description +
                         '<div class="iw-subTitle">Address</div>' +
                         this.address +
-                        '<div class="iw-edit"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
+                        '<div class="iw-edit" style="display: none"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
                         '</div>' +
                         '</div>' +
                         '</div>');
                     //$("<div class='iw-outsidecontent'></div>").wrap("<div class='iw-content'></div>");
+                    if(<? isset($_SESSION['username'])==true ?>){
+                        document.getElementsByClassName("iw-edit").style.display = "";
+                    }
                     infowindow.open(map, this);
                     var areaArray = this.array.split(',');
                     var areaLatLng = [];
@@ -431,14 +434,13 @@ include("scripts/dbconnect.php");
                         '<div class="iw-subTitle">Address</div>' +
                         this.address +
                         '<div class="iw-edit" style="display: none"><a href="/editMapForm/' + this.locationID + '" class="button">Edit</a></div>' +
-                        if(<? isset($_SESSION['username'])==true ?>){
-                            document.getElementsByClassName("iw-edit").style.display = "";
-                        }
                         '</div>' +
                         '</div>' +
                         '</div>');
                     //$("<div class='iw-outsidecontent'></div>").wrap("<div class='iw-content'></div>");
-                    //random comment to hopefully make azure update
+                    if(<? isset($_SESSION['username'])==true ?>){
+                        document.getElementsByClassName("iw-edit").style.display = "";
+                    }
                     infowindow.open(map, this);
                     var routeArray = this.array.split(',');
                     var routeLatLng = [];
