26ed2759ecf05b100d985e31225bc45531affe13
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-11 21:51:58 +0000

    modified picture upload to mysql database

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -1,6 +1,5 @@
 <!--Footer at end of each web page -->
     <footer>
-        <p>(c)2016 - Charlie Bowler, Alexander Fyfe, William Haynes, Scott McMahon</p>
-        <p>Contact Details</p>
+        <p>(c)2016 - CS7</p>
     </footer>
 </body>
