df837a45799b906844b7f1370c884b9c2c3a0cce
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-11 21:49:02 +0000

    modified picture upload to mysql database

--- inc/Forms/clubAdminForm.php
+++ inc/Forms/clubAdminForm.php
@@ -144,100 +144,6 @@ if ($_SESSION['userID']==$_SESSION['adminID'] || $_SESSION['accessLevel'] == '31
         }
         elseif(!empty($_POST['uploadImage'])){
             include("../scripts/dbconnect.php");
-
-
-            // check if a file was submitted
-            if(!isset($_FILES['userfile']))
-            {
-                echo '<p>Please select a file</p>';
-            }
-            else
-            {
-                try {
-                    $msg= upload($db);  //this will upload your image
-                    echo $msg;  //Message showing success or failure.
-                }
-                catch(Exception $e) {
-                    echo $e->getMessage();
-                    echo 'Sorry, could not upload file';
-                }
-            }
-            function upload($db) {
-                include(__DIR__ . "/../scripts/dbconnect.php");
-                session_start();
-                $imageClubID = $_SESSION['clubID'];
-                $maxsize = 10000000; //set to approx 10 MB
-
-                //check associated error code
-                if($_FILES['userfile']['error']==UPLOAD_ERR_OK) {
-
-                    //check whether file is uploaded with HTTP POST
-                    if(is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-
-                        //checks size of uploaded image on server side
-                        if( $_FILES['userfile']['size'] < $maxsize) {
-
-                            //checks whether uploaded file is of image type
-                            //if(strpos(mime_content_type($_FILES['userfile']['tmp_name']),"image")===0) {
-                            $finfo = finfo_open(FILEINFO_MIME_TYPE);
-                            if(strpos(finfo_file($finfo, $_FILES['userfile']['tmp_name']),"image")===0) {
-
-                                // prepare the image for insertion
-                                $imgData =addslashes (file_get_contents($_FILES['userfile']['tmp_name']));
-
-                                // put the image in the db...
-
-                                // our sql query
-                                $sql = "INSERT INTO images(image, name, image_clubID)VALUES('{$imgData}', '{$_FILES['userfile']['name']}', '{$imageClubID}');";
-
-                                // insert the image
-                                mysqli_query($db, $sql);
-                                $msg='<p>Image successfully saved in database with id ='. mysqli_insert_id().' </p>';
-                            }
-                            else
-                                $msg="<p>Uploaded file is not an image.</p>";
-                        }
-                        else {
-                            // if the file is not less than the maximum allowed, print an error
-                            $msg='<div>File exceeds the Maximum File limit</div>
-                <div>Maximum File limit is '.$maxsize.' bytes</div>
-                <div>File '.$_FILES['userfile']['name'].' is '.$_FILES['userfile']['size'].
-                                ' bytes</div><hr />';
-                        }
-                    }
-                    else
-                        $msg="File not uploaded successfully.";
-
-                }
-                else {
-                    $msg= file_upload_error_message($_FILES['userfile']['error']);
-                }
-                return $msg;
-            }
-
-// Function to return error message based on error code
-
-            function file_upload_error_message($error_code) {
-                switch ($error_code) {
-                    case UPLOAD_ERR_INI_SIZE:
-                        return 'The uploaded file exceeds the upload_max_filesize directive in php.ini';
-                    case UPLOAD_ERR_FORM_SIZE:
-                        return 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
-                    case UPLOAD_ERR_PARTIAL:
-                        return 'The uploaded file was only partially uploaded';
-                    case UPLOAD_ERR_NO_FILE:
-                        return 'No file was uploaded';
-                    case UPLOAD_ERR_NO_TMP_DIR:
-                        return 'Missing a temporary folder';
-                    case UPLOAD_ERR_CANT_WRITE:
-                        return 'Failed to write file to disk';
-                    case UPLOAD_ERR_EXTENSION:
-                        return 'File upload stopped by extension';
-                    default:
-                        return 'Unknown upload error';
-                }
-            }/*
-
             $fileName = $_FILES['fileToUpload']['name'];
             $tmpName  = $_FILES['fileToUpload']['tmp_name'];
             $fileSize = $_FILES['fileToUpload']['size'];
@@ -291,7 +197,7 @@ if ($_SESSION['userID']==$_SESSION['adminID'] || $_SESSION['accessLevel'] == '31
             }*/
 
 // Check if $uploadOk is set to 0 by an error
-            /*if ($uploadOk == 0) {
+            if ($uploadOk == 0) {
                 echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
             } else {
@@ -302,7 +208,7 @@ if ($_SESSION['userID']==$_SESSION['adminID'] || $_SESSION['accessLevel'] == '31
                     echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
                 }
 
-            }*/
+            }
         }
     }
 } else {
