30d18ce8d5037dac4df23b10fdef6148c875f8a1
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-12 12:41:20 +0000

    Making map smaller

--- inc/mapPage.php
+++ inc/mapPage.php
@@ -18,7 +18,7 @@ include("scripts/dbconnect.php");
 
         /* Optional: Makes the sample page fill the window. */
         html, body {
-            height: 96.5%;
+            height: 90%;
             margin: 0;
             padding: 0;
         }
