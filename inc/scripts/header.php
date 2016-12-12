c69d125c75a2d12bffe47180dc022df31bcc774c
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-12 14:00:12 +0000

    Changed text

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -10,6 +10,14 @@ session_start(); //start the session
     <title>header</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+    <script type="text/javascript">
+        window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website, cookies are used to help you navigate the site.","dismiss":"Got it!","learnMore":"More info","link":null,"theme":"dark-top"};
+    </script>
+
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+    <!-- End Cookie Consent plugin -->
+
 </head>
 <body style="background-image:url(../../style/background.jpg)">
     <header>
