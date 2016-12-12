c8ecd144e8b346ae49e1fe59bd6f3d39403bbf71
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-12 14:09:55 +0000

    Server security check

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -1,6 +1,5 @@
 <!--Footer at end of each web page -->
     <footer>
         <p>(c)2016 - CS7</p>
-        <a href="http://www.beyondsecurity.com/vulnerability-scanner-verification/cm3028-labenvironment.azurewebsites.net"><img src="https://seal.beyondsecurity.com/verification-images/cm3028-labenvironment.azurewebsites.net/vulnerability-scanner-2.gif" alt="Website Security Test" border="0" /></a>
-    </footer>
+        </footer>
 </body>
