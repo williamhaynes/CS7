f743bfbe06172b045c7ce901bad289f49b7bb3a8
williamhaynes <w.haynes@rgu.ac.uk>
2016-12-12 14:44:59 +0000

    Code tidy up

--- inc/AdminPages/clubAdminPage.php
+++
@@ -1,9 +0,0 @@
-<!--Exclusive to Site Admin & Club Admin: will use a form to allow admins to modify club details-->
-
-<?php
-/**
- * Created by PhpStorm.
- * User: hype_
- * Date: 07/11/2016
- * Time: 12:12
- */
